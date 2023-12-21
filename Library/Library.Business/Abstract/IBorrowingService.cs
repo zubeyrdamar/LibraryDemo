95a47dd471a92551c6b5c0d5d2e6cd1171ea4d25
zubeyr <zubeyrdamar@outlook.com>
2023-12-21 13:31:30 +0300

    Business Layer
    - Services
    - Managers

---
+++ Library/Library.Business/Abstract/IBorrowingService.cs
@@ -0,0 +1,13 @@
+﻿using Library.Entity;
+
+namespace Library.Business.Abstract
+{
+    public interface IBorrowingService
+    {
+        List<Borrowing> List();
+        Borrowing Read(Guid id);
+        void Create(Borrowing Borrowing);
+        void Update(Borrowing Borrowing);
+        void Delete(Guid id);
+    }
+}
--- Library/Library.DataAccess/Abstract/IRepository.cs
+++ Library/Library.DataAccess/Abstract/IRepository.cs
@@ -3,9 +3,9 @@
     public interface IRepository<T>
     {
         IEnumerable<T> List();
-        T Read(int id);
+        T Read(Guid id);
         void Create(T entity);
         void Update(T entity);
-        void Delete(T entity);
+        void Delete(Guid id);
     }
 }
